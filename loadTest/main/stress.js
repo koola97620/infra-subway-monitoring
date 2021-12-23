ef56589806515e8351f0c2c9642c4ccbd9e75541
Lewis Seo <togokr@naver.com>
2021-12-24 06:33:03 +0900

    2단계 - 성능 테스트 (#189)
    * fix(Service): 서비스 요청 로그 추가
    
    - 서비스 요청 로그 추가
    
    * feat(test): 부하 테스트 추가
    
    - 부하 테스트 추가
    - 성능 테스트 답변 작성 중
    
    * feat(test): 부하 테스트 결과 로그 추가
    
    - 부하 테스트 결과 로그 추가
    
    * feat(test): 부하 테스트 결과 패스 추가
    
    - 부하 테스트 결과 패스 추가
    
    * fix(docs): 성능 기준 개선
    
    - 성능 기준 개선
     - 우선 순위로 1순위(FCP)는 가장 좋은 사이트를 따라가려고 목표를 잡고 나머지는 평균으로 하는 것을 목표로 하였다.
     - 너무 큰 목표는 단기 계획에는 단점으로 적용할 수 있다고 생각하여 이렇게 목표를 잡게 되었다.
    
    * feat(test): update 테스트 스크립트 작성
    
    - update 테스트 스크립트 작성
    
    * fix(test): update load 테스트 수치 조정
    
    - update load 테스트 수치 조정
    
    * fix(test): update stress 테스트 수치 조정
    
    - update stress 테스트 수치 조정
    
    * fix(test): update 테스트 결과 반영
    
    - update 테스트 결과 반영
    
    * fix(test): update stress 결과 좀 더 곡선으로 증가하도록 변경
    
    - update stress 결과 좀 더 곡선으로 증가하도록 변경
    
    * fix(test): update stress 결과 합체
    
    - update stress 결과 합체
    
    * fix(test): 메인페이지, path 페이지 stress 수정된 vuser 적용
    
    - 메인페이지, path 페이지 stress 수정된 vuser 적용
    
    * fix(test): 메인페이지 stress 테스트 수치 증가
    
    - 메인페이지 stress 테스트 수치 증가
    
    * fix(test): 메인 페이지 테스트 http_req_duration  조정
    
    * fix(test): 메인페이지 stress 로그 기록
    
    - 메인페이지 stress 로그 기록
    
    * fix(test): 메인페이지 load 로그 기록
    
    - 메인페이지 load 로그 기록
    
    * fix(test): path source target 설정
    
    * fix(test): path load, stress 테스트 로그 기록
    
    - path load, stress 테스트 로그 기록

---
+++ loadTest/main/stress.js
@@ -0,0 +1,27 @@
+import http from 'k6/http';
+import { check, group, sleep, fail } from 'k6';
+
+export let options = {
+  stages: [
+    { duration: '2s', target: 300 }, // below normal load
+    { duration: '10s', target: 3000 },
+    { duration: '10s', target: 1000 },
+    { duration: '10s', target: 1000 },
+    { duration: '5s', target: 0 }, // scale down. Recovery stage.
+  ],
+  thresholds: {
+    http_req_duration: ['p(99)<3'], // 99% of requests must complete below 1.5s
+    'logged in successfully': ['p(99)<3'], // 99% of requests must complete below 1.5s
+  },
+};
+
+const BASE_URL = 'http://3.36.50.109:8080';
+const USERNAME = 'abc@abc.com';
+const PASSWORD = '123123';
+
+export default function ()  {
+
+  http.get(`${BASE_URL}`);
+
+  sleep(1);
+};
---
+++ loadTest/member_update/stress.js
@@ -0,0 +1,60 @@
+import http from 'k6/http';
+import { check, group, sleep, fail } from 'k6';
+
+export let options = {
+  stages: [
+    { duration: '2s', target: 300 }, // below normal load
+    { duration: '10s', target: 600 },
+    { duration: '10s', target: 700 },
+    { duration: '10s', target: 1000 },
+    { duration: '3s', target: 0 }, // scale down. Recovery stage.
+  ],
+  thresholds: {
+    http_req_duration: ['p(99)<1500'], // 99% of requests must complete below 1.5s
+    'logged in successfully': ['p(99)<1500'], // 99% of requests must complete below 1.5s
+  },
+};
+
+const BASE_URL = 'http://3.36.50.109:8080';
+const USERNAME = 'abc@abc.com';
+const PASSWORD = '123123';
+
+export default function ()  {
+
+  var payload = JSON.stringify({
+    email: USERNAME,
+    password: PASSWORD,
+  });
+
+  var params = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+
+  let loginRes = http.post(`${BASE_URL}/login/token`, payload, params);
+
+  check(loginRes, {
+    'logged in successfully': (resp) => resp.json('accessToken') !== '',
+  });
+
+
+  let authHeaders = {
+    headers: {
+      Authorization: `Bearer ${loginRes.json('accessToken')}`,
+    },
+  };
+
+  var memberUpdate = JSON.stringify({
+                         email: "updated",
+                         password: "PASSWORD",
+                         age : "12"
+  });
+
+  let myObjects = http.put(`${BASE_URL}/members/me`, memberUpdate, authHeaders).json();
+
+  check(myObjects, { 'retrieved member': (obj) => obj.id != 0 });
+
+  sleep(1);
+};
